650286f9aa84583ff714903555998ee854b42f7b
Dave Horton <daveh@beachdognet.com>
2018-11-12 21:11:48 -0500

    update and simplify

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
-'use strict';
+const Srf = require('drachtio-srf') ;
+const srf = new Srf();
+const config = require('config');
+const pino = require('pino');
+const level = config.has('log.level') ? config.get('log.level') : 'info';
+const logger = srf.locals.logger = pino({level});
+const inviteHandler = require('./lib/invite')({logger, srf});
 
-var drachtio = require('drachtio') ;
-var app = drachtio() ;
-var config = app.config = require('./config');
-var fs = require('fs') ;
-var rangeCheck = require('range_check');
-var debug = app.debug = require('debug')('simple-sip-proxy') ;
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
 
-// Expose app
-exports = module.exports = app;
+srf.connect(config.get('drachtio'))
+  .on('connect', (err, hp) => {
+    if (err) return logger.error(err, 'error connecting');
+    logger.info(`connected to drachtio server at ${hp}`);
+  })
+  .on('error', (err) => {
+    logger.error(err, 'Error connecting to drachtio server');
+  });
 
-//connect to drachtio server
-app.connect({
-  host: config.drachtioServer.address,
-  port: config.drachtioServer.port,
-  secret: config.drachtioServer.secret,
-}) ;
-
-app.on('connect', function(err, hostport) {
-  if( err ) throw err ;
-  console.log('connected to drachtio server at %s', hostport) ;
-})
-.on('error', function(err){
-  console.warn(err.message ) ;
-}) ;
-
-app.use(function checkSender(req, res, next) {
-  if( !rangeCheck.inRange( req.source_address, app.config.authorizedSources) ) { return res.send(403) ; }
-  next() ;
-}) ;
-app.use(function onlyInvites( req, res, next ) { 
-  if( -1 === ['invite','cancel','prack','ack'].indexOf( req.method.toLowerCase() ) ) { return res.send(405); }
-  next() ;
-}); 
-
-require('./lib/proxy.js')(app); 
-
-//handle any errors thrown while executing middleware stack
-app.use(function myErrorHandler(err, req, res, next) {
-  debug('caught error generated from middleware: ', err); 
-  res.send(500, {
-    headers: {
-      'X-Error-Info': err.message || 'unknown application error'
-    }
-  }) ;
-}) ;
-
-
+srf.invite(inviteHandler);
